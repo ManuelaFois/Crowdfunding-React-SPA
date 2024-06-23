38ffd57b4198107b67c8efae19c94982645bf6a0
ManuelaFois <manuelafoisdtp@gmail.com>
2024-06-24 00:56:04 +0200

    Modified dropdown menu for blog titles

--- src/Components/Layout/titlesPostsCol.jsx
+++ src/Components/Layout/titlesPostsCol.jsx
@@ -8,24 +8,20 @@ export default function ListPosts() {
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef(null);
 
-  // Funzione per gestire l'apertura e la chiusura del menu
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
-  // Funzione per chiudere il menu
   const closeMenu = () => {
     setIsOpen(false);
   };
 
-  // Gestore per chiudere il menu quando si clicca fuori
   const handleClickOutside = (event) => {
     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
       setIsOpen(false);
     }
   };
 
-  // Effetto per aggiungere l'ascoltatore di eventi quando il componente è montato
   useEffect(() => {
     document.addEventListener("click", handleClickOutside, true);
     return () => {
@@ -34,8 +30,8 @@ export default function ListPosts() {
   }, []);
 
   return (
-    <>
-      <button className="btn btn-dark col-4 mb-2 button-custom" onClick={toggleMenu}>
+    <div className="dropdown-container p-0">
+      <button className="btn btn-dark mb-2 button-custom" onClick={toggleMenu}>
         {"TITOLI"}<img src={arrow} alt="arrow icon" />
       </button>
       <div
@@ -43,54 +39,32 @@ export default function ListPosts() {
         className={`dropdown-menu dropdown-menu-custom ${isOpen ? "show" : ""}`}
       >
         <div className="dropdown-item">
-          <Link
-            to="/Post1"
-            className={({ isActive }) => (isActive ? "link active" : "link")}
-           
-          >
+          <Link to="/Post1" className="link">
             {"L'importanza della natura per i bambini"}
           </Link>
         </div>
 
         <div className="dropdown-item">
-          <Link
-            to="/Post2"
-            className={({ isActive }) => (isActive ? "link active" : "link")}
-          >
+          <Link to="/Post2" className="link">
             {"La carenza di relazione con la natura e gli animali nei bambini"}
           </Link>
         </div>
 
         <div className="dropdown-item">
-          <Link
-            to="/Post3"
-            className={({ isActive }) => (isActive ? "link active" : "link")}
-          >
-            {
-              "Il nostro evento ha riscosso un gran successo, la raccolta fondi prosegue"
-            }
+          <Link to="/Post3" className="link">
+            {"Il nostro evento ha riscosso un gran successo, la raccolta fondi prosegue"}
           </Link>
         </div>
 
         <div className="dropdown-item">
-          <Link
-            to="/Post4"
-            className={({ isActive }) => (isActive ? "link active" : "link")}
-          >
-            {
-              "L'importanza di ristabilire il legame perduto tra uomo, animali e natura"
-            }
+          <Link to="/Post4" className="link">
+            {"L'importanza di ristabilire il legame perduto tra uomo, animali e natura"}
           </Link>
         </div>
 
         <div className="dropdown-item">
-          <Link
-            to="/Post5"
-            className={({ isActive }) => (isActive ? "link active" : "link")}
-          >
-            {
-              "Inquinamento: l'urgenza di agire per il futuro del nostro pianeta"
-            }
+          <Link to="/Post5" className="link">
+            {"Inquinamento: l'urgenza di agire per il futuro del nostro pianeta"}
           </Link>
         </div>
 
@@ -98,6 +72,6 @@ export default function ListPosts() {
           <button className="btn btn-link px-0" onClick={closeMenu}>Chiudi</button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
